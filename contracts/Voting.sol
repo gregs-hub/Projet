e876d28a93ade2b773cb67d56b33fccde593649a
gregs-hub <36857267+gregs-hub@users.noreply.github.com>
2022-04-14 11:56:24 +0200

    Update contract with natspec and fix DoS

--- contracts/Voting.sol
+++ contracts/Voting.sol
@@ -1,30 +1,37 @@
 // Voting.sol
 // SPDX-License-Identifier: MIT
-pragma solidity ^0.8.12;
+pragma solidity 0.8.13;
 
-import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/master/contracts/access/Ownable.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
 
-/*
- * @title Voting contract for a small organization
- * @author Grégory Seiller
- */
-
+/// @title Voting contract
+/// @author Grégory Seiller
+/// @notice Voting contract for a small organization
 contract Voting is Ownable {
 
-    // STATE VARIABLES
+    // STATES VARIABLES
+
+    /// @notice Winning proposal Id
+    /// @dev Stores the winning proposal
+    uint public winningProposalID;
+
+    /// @dev Defines a voter
     struct Voter {
         bool isRegistered;
         bool hasVoted;
         uint votedProposalId;
     }
 
+    /// @dev Defines a proposal
     struct Proposal {
         string description;
         uint voteCount;
     }
-    Proposal[] proposals;
-    uint proposalId;
 
+    /// @dev Array of proposals
+    Proposal[] proposals;
+
+    /// @dev List of statuses
     enum WorkflowStatus {
         RegisteringVoters,
         ProposalsRegistrationStarted,
@@ -33,123 +40,146 @@ contract Voting is Ownable {
         VotingSessionEnded,
         VotesTallied
     }
-    WorkflowStatus status = WorkflowStatus.RegisteringVoters;
 
-    mapping(address => bool) public whitelist;
-    mapping(address => Voter) public voters;
+    /// @dev Status stage
+    WorkflowStatus status;
 
-    uint public nbVoters;
-    uint public nbVotes;
-    uint maxVotesCount;
-    uint[] maxVotesId;
+    /// @dev Whitelist of voters
+    mapping(address => Voter) voters;
+
 
     // EVENTS
+
+    /// @dev Voter registration event
+    /// @param voterAddress Voter's address
     event VoterRegistered(address voterAddress);
+
+    /// @dev Worflow status change event
+    /// @param previousStatus Previous status
+    /// @param newStatus New status
     event WorkflowStatusChange(WorkflowStatus previousStatus, WorkflowStatus newStatus);
+
+    /// @dev Proposal registration event
+    /// @param proposalId Proposal Id
     event ProposalRegistered(uint proposalId);
-    event Voted (address voter, uint proposalId);
+
+    /// @dev New vote event
+    /// @param voter Voter's address
+    /// @param proposalId Proposal Id
+    event Voted(address voter, uint proposalId);
+
 
     // MODIFIERS
-    // onlyOwner herited from Ownable.sol
+
+    /// @dev Modifier to check if current sender is registered as a voter
     modifier isWhitelisted() {
-        require(whitelist[msg.sender] == true, "You are not in the whitelist, cannot proceed");
+        require(voters[msg.sender].isRegistered, "You are not in the whitelist, cannot proceed");
         _;
     }
 
+    /// @dev Modifier to check if current status is the one expected
+    /// @param _status Expected status
     modifier withStatus(uint _status) {
         require(uint(status) == _status, "Impossible during this phase");
         _;
     }
 
-    // FUNCTIONS
-    // constructor() {}
 
-    // At any time, whitelisted voters can view all listed proposals
-    function showProposals() external view isWhitelisted returns(Proposal[] memory)  {
+    // FUNCTIONS
+
+    /// @notice Get voter's details based on his address
+    /// @dev At any time, whitelisted voters can view a voter based on address
+    /// @param _address Voter's address
+    /// @return Voter's structure
+    function getVoter(address _address) external view isWhitelisted returns (Voter memory) {
+        return voters[_address];
+    }
+
+    /// @notice Get all proposals
+    /// @dev At any time, whitelisted voters can view all listed proposals
+    /// @return Array of proposals
+    function getProposals() external view isWhitelisted returns(Proposal[] memory)  {
         return proposals;
     }
 
-    // At any time, whitelisted voters can view current status
-    function showStatus() external view isWhitelisted returns(WorkflowStatus) {
+    /// @notice Get current status
+    /// @dev At any time, whitelisted voters can view current status
+    /// @return Workflow status
+    function getStatus() external view isWhitelisted returns(WorkflowStatus) {
         return status;
     }
 
-    // Owner can add a new voter to the whitelist. Current status must be RegisteringVoters
+    /// @notice Owner can add a new voter
+    /// @dev Owner can add a new voter to the whitelist. Current status must be RegisteringVoters. Emits VoterRegistered
+    /// @param _address Voter's address
     function addVoter(address _address) external onlyOwner withStatus(0) {
-        require(!whitelist[_address], "This voter already exists");
-        whitelist[_address] = true;
-        voters[_address] = Voter(true, false, 0);
-        nbVoters++;
+        require(voters[_address].isRegistered != true, "This voter already exists");
+        voters[_address].isRegistered = true;
         emit VoterRegistered(_address);
     }
 
-    // Owner starts the proposal registration phase. Current status must be RegisteringVoters
+    /// @notice Owner starts the proposal registration phase
+    /// @dev Owner starts the proposal registration phase. Current status must be RegisteringVoters. Emits WorkflowStatusChange
     function startProposalRegistration() external onlyOwner withStatus(0) {
-        require(nbVoters >= 1, "No voter found in your whitelist registry");
         status = WorkflowStatus.ProposalsRegistrationStarted;
         emit WorkflowStatusChange(WorkflowStatus.RegisteringVoters, status);
     }
 
-    // Whitelisted voters can add a new proposal. Current status must be ProposalsRegistrationStarted
+    /// @notice Voter can add a new proposal
+    /// @dev Whitelisted voters can add a new proposal. Current status must be ProposalsRegistrationStarted. Emits ProposalRegistered
+    /// @param _description Proposal description
     function addProposal(string memory _description) external isWhitelisted withStatus(1) {
         require(bytes(_description).length > 0, "Cannot accept an empty proposal description");
-        Proposal memory proposal = Proposal(_description, 0);
+        Proposal memory proposal;
+        proposal.description = _description;
         proposals.push(proposal);
-        proposalId++;
-        emit ProposalRegistered(proposalId);
+        emit ProposalRegistered(proposals.length-1);
     }
 
-    // Owner ends the proposal registration phase. Current status must be ProposalsRegistrationStarted
+    /// @notice Owner ends the proposal registration phase
+    /// @dev Owner ends the proposal registration phase. Current status must be ProposalsRegistrationStarted. Emits WorkflowStatusChange
     function endProposalRegistration() external onlyOwner withStatus(1) {
-        require(proposalId >= 1, "There is no submitted proposal for now, cannot end this phase");
+        require(proposals.length >= 1, "There is no submitted proposal for now, cannot end this phase");
         status = WorkflowStatus.ProposalsRegistrationEnded;
         emit WorkflowStatusChange(WorkflowStatus.ProposalsRegistrationStarted, status);
     }
 
-    // Owner starts the voting phase. Current status must be ProposalsRegistrationEnded
+    /// @notice Owner starts the voting phase
+    /// @dev Owner starts the voting phase. Current status must be ProposalsRegistrationEnded. Emits WorkflowStatusChange
     function startVotingSession() external onlyOwner withStatus(2) {
         status = WorkflowStatus.VotingSessionStarted;
         emit WorkflowStatusChange(WorkflowStatus.ProposalsRegistrationStarted, status);
     }
 
-    // Whitelisted voters can vote for a unique proposal. Current status must be VotingSessionStarted
-    function doVote(uint _proposalId) external isWhitelisted withStatus(3) {
+    /// @notice Voter can vote for a proposal
+    /// @dev Whitelisted voters can vote for a unique proposal. Current status must be VotingSessionStarted. Emits Voted
+    /// @param _proposalId Proposal Id
+    function setVote(uint _proposalId) external isWhitelisted withStatus(3) {
         require(!voters[msg.sender].hasVoted, "Already voted");
-        require(_proposalId < proposalId, "No proposal with this Id");
         voters[msg.sender].hasVoted = true;
         voters[msg.sender].votedProposalId = _proposalId;
         proposals[_proposalId].voteCount++;
-        nbVotes++;
         emit Voted (msg.sender, _proposalId);
     }
 
-    // Owner ends the voting phase. Current status must be VotingSessionStarted
+    /// @notice Owner ends the voting phase
+    /// @dev Owner ends the voting phase. Current status must be VotingSessionStarted. Emits WorkflowStatusChange
     function endVotingSession() external onlyOwner withStatus(3) {
-        require(nbVotes >= 1, "There is no vote for now, cannot end this phase");
         status = WorkflowStatus.VotingSessionEnded;
         emit WorkflowStatusChange(WorkflowStatus.VotingSessionStarted, status);
     }
 
-    // Owner tallies the final votes. Current status must be VotingSessionEnded
+    /// @notice Owner tallies the final votes
+    /// @dev Owner tallies the final votes. Current status must be VotingSessionEnded. Maximum 100 proposals (DoS). Emits WorkflowStatusChange
     function tallyVotes() external onlyOwner withStatus(4) {
-        for (uint i = 0; i < proposals.length ; i++) {
-            if (proposals[i].voteCount > maxVotesCount) {
-                maxVotesCount = proposals[i].voteCount;
-                delete maxVotesId;
-                maxVotesId.push(i);
-            }
-            else if (proposals[i].voteCount == maxVotesCount) {
-                maxVotesId.push(i);
+        uint _winningProposalId;
+        for (uint256 p = 0; p < 100; p++) {
+            if (proposals[p].voteCount > proposals[_winningProposalId].voteCount) {
+                _winningProposalId = p;
             }
         }
+        winningProposalID = _winningProposalId;
         status = WorkflowStatus.VotesTallied;
         emit WorkflowStatusChange(WorkflowStatus.VotingSessionEnded, status);
-    }
-
-    // Owner publishes the final winning proposal, or an error if no majority. Current status must be VotesTallied
-    function getWinner() external view withStatus(5) returns(uint winningProposalId, string memory) {
-        require(maxVotesId.length == 1, "No majority found, new vote will be created with the tied proposals");
-        winningProposalId = maxVotesId[0];
-        return (winningProposalId, proposals[winningProposalId].description);
     }
 }
